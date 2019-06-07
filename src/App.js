3b0688a780ef829b6a0992118de4be3ccfc033f9
jhony maurad <jhonymaurad@gmail.com>
2019-06-06 23:24:02 -0400

    Add css, Clean code, Add conditional rendering

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import './App.css';
 import ZipList from './ZipList';
 
-const BASE_URL = 'http://ctp-zip-api.herokuapp.com/city/BRONX';
+const BASE_URL = 'http://ctp-zip-api.herokuapp.com/city/';
 
 class App extends React.Component {
 
@@ -17,7 +17,8 @@ class App extends React.Component {
   }
 
   handleSubmit =()=>{
-    const data = axios.get(BASE_URL)
+    const url = BASE_URL + this.state.city.toUpperCase();
+    const data = axios.get(url)
     .then(resp => {this.setState({zipcodes: resp.data})},
     (error)=> {this.setState({errorMessage: error.message})});
   }
@@ -27,11 +28,11 @@ class App extends React.Component {
   }
 
   render(){
+    if(this.state.errorMessage) return <h1>{this.state.errorMessage}</h1>
     return (
       <div className="App">
         <label>Enter city name: </label>
         <input type="text" onChange={this.handleChange}></input>
-
         <button onClick={this.handleSubmit}>Submit</button>
 
         <ZipList zipcodes={this.state.zipcodes}/>
--- src/ZipList.js
+++ src/ZipList.js
@@ -2,20 +2,15 @@ import React from 'react';
 
 const ZipList =(props)=>{
     return(
-        <div>
+        <div className="zip-list-container">
             <h1>list of zip codes</h1>
             {props.zipcodes.map(zip =>(
-                <div key={zip}>
+                <div key={zip} className="zip-container">
                     <h2>{zip}</h2>
                 </div>
             ))}
-
         </div>
     )
-        
-
-    // props.zipcodes.map()
-
 }
 
 export default ZipList;
