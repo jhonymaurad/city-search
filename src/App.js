1dc0d2dead6f77b976cea3ee8bee6c888dd99e62
jhony maurad <jhonymaurad@gmail.com>
2019-06-06 22:11:31 -0400

    Add Axios and cleaned unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const BASE_URL = 'http://ctp-zip-api.herokuapp.com/city/BRONX';
+
+class App extends React.Component {
+
+  handleSubmit =()=>{
+    
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        hello world
+
+        <button onClick={this.handleSubmit}>Submit</button>
+      </div>
+    );
+
+  }
+  
 }
 
 export default App;
